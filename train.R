f0317d331151cecf6abab5353ca7cb93a742a28e
Daniel Falbel <dfalbel@gmail.com>
2023-02-14 08:21:48 +0100

    Improve training.

--- train.R
+++ train.R
@@ -1,5 +1,5 @@
 #| requires:
-#|  - file: pets
+#|  - file: data
 #|    target-type: link
 #| sourcecode:
 #|  - "*.R"
@@ -9,8 +9,9 @@
 
 box::use(luz[...])
 box::use(./diffusion[diffusion_model])
-box::use(./dataset[train_ds, debug_ds])
+box::use(./dataset[make_dataset])
 box::use(torch[...])
+box::use(./callbacks[callback_generate_samples])
 
 block_depth <- 2
 lr <- 1e-3
@@ -21,6 +22,7 @@ min_signal_rate <- 0.02
 max_signal_rate <- 0.95
 patience <- 100
 weight_decay <- 1e-4
+dataset_name <- "flowers"
 
 image_size <- c(3 , 32, 32)
 
@@ -32,7 +34,7 @@ optimizer <- if (optimizer == "adam") {
   rlang::abort("Optimizer not currently supported.")
 }
 
-dataset <- train_ds
+dataset <- make_dataset(dataset_name, image_size[-1])
 
 model <- diffusion_model %>%
   setup(optimizer = optimizer) %>%
@@ -43,15 +45,16 @@ model <- diffusion_model %>%
   ) %>%
   set_opt_hparams(lr = lr, weight_decay = weight_decay)
 
-finder <- luz::lr_finder(model, data = dataset)
-plot(finder) + ggplot2::coord_cartesian(ylim = c(0, 2))
+# finder <- luz::lr_finder(model, data = dataset)
+# plot(finder) + ggplot2::coord_cartesian(ylim = c(0, 2))
 
 fitted <- model %>%
   fit(
     dataset, epochs = epochs, dataloader_options = list(batch_size = batch_size), verbose = TRUE,
     callbacks = list(
       luz_callback_lr_scheduler(lr_step, step_size = patience, gamma = 0.1^(1/3)),
-      luz_callback_early_stopping(monitor = "train_loss", min_delta = 0.0005, patience = patience)
+      luz_callback_early_stopping(monitor = "train_loss", min_delta = 0.0005, patience = patience),
+      callback_generate_samples(num_images = 20, diffusion_steps = 20)
     )
   )
 
